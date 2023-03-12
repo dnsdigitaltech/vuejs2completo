4fc603bba1fde57fe0f33ae339ed2748bdea1a5a
Davi Bernardo <davi.bernardo@sbahq.org>
2023-03-12 11:08:19 -0300

    Criando um componente no projeto

---
+++ projeto-simples/src/Announcement.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-info">
+        {{ announcenment }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                announcenment: 'O site será desativado para manutenção em breve!'
+            };
+        }
+    }
+</script>
--- projeto-simples/src/main.js
+++ projeto-simples/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Announcement from './Announcement.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.component('announcement', Announcement);
 
 new Vue({
   el: '#app',
