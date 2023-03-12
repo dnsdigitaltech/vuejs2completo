c577f8f33b769dbcae340e311329854776bd47c8
Davi Bernardo <davi.bernardo@sbahq.org>
2023-03-12 18:28:26 -0300

    Alternando o menu para ativo entre visualizações

--- sistema-email-app/src/Content.vue
+++ sistema-email-app/src/Content.vue
@@ -1,8 +1,52 @@
 <template>
-    <h1>Conteudo</h1>
+    <aside class="lg-side">
+        <div class="inbox-head">
+            <h3>{{ currenView.title }}</h3>
+        </div>
+        <keep-alive>
+            <component :is="currenView.tag"></component>
+        </keep-alive>
+        
+    </aside>
 </template>
 <script>
+    import Inbox from './Inbox.vue';
+    import Sent from './Sent.vue';
+    import Important from './Important.vue';
+    import Trash from './Trash.vue';
+    import ViewMessage from './ViewMessage.vue';
+    import { eventBus } from './main';
     export default {
-        
+        created() {
+            eventBus.$on('changeView', (data) => {
+                let temp = [{
+                    tag: data.tag,
+                    title: data.title
+                }];
+                this.history = temp.concat(this.history.splice(0));
+            })
+        },
+        data() {
+            return {
+                history: [
+                    {
+                        tag: 'app-inbox',
+                        title: 'Inbox'
+                    }
+                ]
+            };
+        },
+        computed: {
+            currenView() {
+                return this.history[0];
+            }
+        },
+        components: {
+           appInbox: Inbox,
+           appSent: Sent,
+           appImportant: Important,
+           appTrash: Trash,
+           appViewMessage: ViewMessage
+        }
     }
 </script>
