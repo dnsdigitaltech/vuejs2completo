bc92bfdaf30e10c91aa122ade323c8bee81a3fdd
Davi Bernardo <davi.bernardo@sbahq.org>
2023-03-13 08:21:19 -0300

    Carregando mensagens do arquivo

--- sistema-email-app/src/App.vue
+++ sistema-email-app/src/App.vue
@@ -10,7 +10,13 @@
 <script>
     import Sidebar from './Sidebar.vue';
     import Content from './Content.vue';
+    import message from './data/messages';
     export default {
+        data() {
+            return {
+                messages: message
+            };
+        },
         components: {
             appSidebar: Sidebar,
             appContent: Content
